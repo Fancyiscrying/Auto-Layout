ec0c31026655255e1abb7e233008c1d457be37cf
Fancyiscrying <Fancy@fancydeair.mshome.net>
2016-02-25 14:47:10 +0800

    Initial Commit

---
+++ Auto Layout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Auto Layout
+//
+//  Created by Fancy on 16/2/25.
+//  Copyright © 2016年 Fancy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
